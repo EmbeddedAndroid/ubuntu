d9a21815dfdf8bf64a32956eae151eb2e1a440da
Tyler Baker <tyler.baker@linaro.org>
2016-08-25 15:44:55 -0700

    Add local-dev hacks

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Fetching LAVA API Key Started $(date +"%T")" >> /var/www/html/seq.txt
-wget http://localhost/tmp/apikey.txt
+wget http://lava/tmp/apikey.txt
 echo "Fetching LAVA API Key Finished $(date +"%T")" >> /var/www/html/seq.txt
 echo "Running Tests Started $(date +"%T")" >> /var/www/html/seq.txt
+sed -i -e "s/localhost/$(hostname -I | tr -d '[[:space:]]')/g" *.yaml
 ./submityaml.py -k apikey.txt -p aes128-test.yaml
 echo "Running Tests Finished $(date +"%T")" >> /var/www/html/seq.txt
 cat results.txt >> /var/www/html/seq.txt
