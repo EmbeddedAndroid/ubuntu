e26643bf43d9542e59bc88b560249c254b967a96
Tyler Baker <tyler.baker@linaro.org>
2016-08-26 10:59:38 -0700

    Add first set of kernel tests

--- run-tests.sh
+++ run-tests.sh
@@ -10,6 +10,6 @@ echo "Fetching LAVA API Key Finished $(date +"%T")" >> /var/www/html/seq.txt
 echo "Running Tests Started $(date +"%T")" >> /var/www/html/seq.txt
 sed -i -e "s/localhost/$(hostname -I | tr -d '[[:space:]]')/g" *.yaml
 sed -i -e "s/{PLATFORM}/$ZEPHYR_PLATFORM/g" *.yaml
-./submityaml.py -k apikey.txt -p aes128-test.yaml aes128-cbc-test.yaml ccm-mode-test.yaml cmac-mode-test.yaml aes128-ctr-mode-test.yaml hmac-rfc4231-vectors-test.yaml mbedtls-test.yaml hmac-prng-test.yaml 
+./submityaml.py -k apikey.txt -p aes128-test.yaml aes128-cbc-test.yaml ccm-mode-test.yaml cmac-mode-test.yaml aes128-ctr-mode-test.yaml hmac-rfc4231-vectors-test.yaml mbedtls-test.yaml hmac-prng-test.yaml nano-lifo-test.yaml nano-work-test.yaml microkernel-memory-map-test.yaml microkernel-memory-pool-test.yaml vim microkernel-mutex-api-test.yaml 
 echo "Running Tests Finished $(date +"%T")" >> /var/www/html/seq.txt
 cat results.txt >> /var/www/html/seq.txt
